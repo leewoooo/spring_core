1fec0f3f39a595bac2052cf767f25ddc82107117
leewoooo <leecoding2285@gmail.com>
2022-02-06 23:57:19 +0900

    initial commit

---
+++ core-noref/src/main/java/hello/corenoref/CoreNorefApplication.java
@@ -0,0 +1,13 @@
+package hello.corenoref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreNorefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreNorefApplication.class, args);
+	}
+
+}
